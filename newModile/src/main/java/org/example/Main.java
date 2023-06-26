3aad1ced9e1d251a5c1939aa8348eaa3c8b2337d
Pavel Khudenko <khudenk0pave1@gmail.com>
2023-06-26 11:45:42 +0300

    firs commit

---
+++ newModile/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
